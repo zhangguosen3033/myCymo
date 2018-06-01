f5841744ef90ca4b08338823a2313a62f7da7002
fjs <fjs@fjsdeMac-mini.local>
2018-06-01 13:18:27 +0800

    no message

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
+  API_ROOT: '"这里需要对应的接口地址"',
   API_TOKEN: '"bearer 467405f6-331c-4914-beb7-42027bf09a01"'
 })
